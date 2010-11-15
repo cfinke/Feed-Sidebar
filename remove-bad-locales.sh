4a5916a71d36554b9088f16ad487b5b0780e87b0
Christopher Finke <cfinke@gmail.com>
2010-11-15 02:14:28 +0000

    Script for removing incomplete locales from babelzilla

---
+++ remove-bad-locales.sh
@@ -0,0 +1,7 @@
+rm -rf feedsidebar/chrome/locale/de/
+rm -rf feedsidebar/chrome/locale/nl/
+rm -rf feedsidebar/chrome/locale/ja/
+rm -rf feedsidebar/chrome/locale/fr/
+rm -rf feedsidebar/chrome/locale/vi-VN/
+
+git checkout feedsidebar/chrome/locale/en-US/
