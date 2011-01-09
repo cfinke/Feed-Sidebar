45c27376aa3c38b8acc85f67c1ba05fad5097d31
Christopher Finke <cfinke@gmail.com>
2011-01-09 03:09:41 +0000

    Updated locales

--- remove-bad-locales.sh
+++ remove-bad-locales.sh
@@ -1,7 +1,3 @@
-rm -rf feedsidebar/chrome/locale/de/
-rm -rf feedsidebar/chrome/locale/nl/
-rm -rf feedsidebar/chrome/locale/ja/
-rm -rf feedsidebar/chrome/locale/fr/
-rm -rf feedsidebar/chrome/locale/vi-VN/
+rm -rf feedsidebar/chrome/locale/{ca,da,pl,de,nl,ja,fr,vi-VN}/
 
 git checkout feedsidebar/chrome/locale/en-US/
